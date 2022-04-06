5575136718a58946d10ff0b84fa8ec22b297c427
Mojtabaa Alipour <66869651+mipxman@users.noreply.github.com>
2022-04-06 22:48:18 +0430

    Add files via upload
    this program get 10 numbers from user and then find the  numerous of prime divisor of a entry numbers and print the maximum one .

---
+++ Prime_Divisor.py
@@ -0,0 +1,41 @@
+'''
+Mojtaba Alipour ---- Find Prime Divisor between 10 numbers 
+'''
+t = 0
+count = 0 #counter 
+ans = 0 #answer
+list = [] # entry list
+prime = []  #prime_number list 
+
+
+for i in range(1,11):
+    number = int(input(''))
+    list.append(number)
+
+#find maximum number 
+max_num=(max(list))
+
+#find_prime_list
+for numy in range (1, max_num):  
+    if numy > 1:  
+        for i in range (2, numy):  
+            if (numy % i) == 0:  
+                break  
+        else:  
+            prime.append(numy) 
+
+
+
+for y in list:
+    for x in prime:
+        if (y % x == 0):
+            t+=1
+    if t > count :
+        count = t 
+        ans = y
+    elif t == count:
+        if( y > ans):
+            ans = y
+            count = t
+    t = 0
+print(ans , count )
